31d0994910e82137417bd19e81f76362adfeb72f
yeahwouh <michaelauer1010@gmail.com>
2024-10-11 00:43:43 +0200

    Projects bar (visually) updates according to the projects list of ToDoEntry appended ScreenController as a factory function, updateScreen by now just updates the projects bar

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@
 import "./styles.css"
 import { ToDoEntry } from "./listLogic"
 
+function ScreenController() {
+    let toDos = ToDoEntry.toDos;
+    let projects = ToDoEntry.projects;
+    const projectsBar = document.querySelector(".projects");
+    const content = document.querySelector(".content-box")
+
+    const updateScreen = () => {
+        // Updating the projects bar
+        for (let project in projects) {
+            let listElement = document.createElement("li");
+            listElement.textContent = project;
+            projectsBar.appendChild(listElement);
+        }
+    }
+
+    // Initial update
+    updateScreen();
+}
+
+ScreenController();
